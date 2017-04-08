235adf18a3b606a9922d58295c1c1339fe14ef90
初コミット <kikuchitatsuya@kikuchitatsuya-no-MacBook-Air.local>
2017-04-08 22:07:07 +0900

    added room edit and new form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,4 +8,20 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.create(room_params)
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.update(room_params)
+  end
 end
