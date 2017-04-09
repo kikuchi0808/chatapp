64384a3db26a4721cfeb4fb0188685c614e292cf
初コミット <kikuchitatsuya@kikuchitatsuya-no-MacBook-Air.local>
2017-04-09 18:19:27 +0900

    added device function still need to fix design for this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 # rubocop:disable Metrics/LineLength
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
   before_action :set_rooms
 
   private
